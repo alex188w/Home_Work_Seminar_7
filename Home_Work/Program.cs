66103f1ae1d98e5b9f56374d12188d7dc5309f44
Алексей Шевченко <alex18w@mail.ru>
2022-08-15 22:16:32 +0500

    Задачи 47, 50, 52

--- Home_Work/Program.cs
+++ Home_Work/Program.cs
@@ -16,6 +16,50 @@
     PrintArrayDouble(array);
 }
 
+void Zadacha50()
+// Напишите программу, которая на вход принимает позиции элемента
+// в двумерном массиве, и возвращает значение этого элемента или же указание,
+// что такого элемента нет
+{
+    Random random = new Random();
+    int rows = random.Next(4, 10);
+    int colums = random.Next(4, 10);
+    int[,] array = new int[rows, colums];
+    FillArray(array, 0, 10);
+    Console.WriteLine("Заданный массив ");
+    PrintArray(array);
+
+    Console.WriteLine("Введите номер строки нужного элемента ");
+    int m = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите номер столбца нужного ");
+    int n = Convert.ToInt32(Console.ReadLine());
+    if((m-1) >= rows || (n-1) >= colums) Console.WriteLine("Такого элемента нет в массиве ");
+    else Console.WriteLine("Значение данного элемента: " + array[m-1, n-1]);
+}
+
+void Zadacha52()
+// Задайте двумерный массив из целых чисел. Найдите среднее арифметическое 
+// элементов в каждом столбце
+{
+    Random random = new Random();
+    int rows = random.Next(4, 8);
+    int colums = random.Next(4, 8);
+    int[,] array = new int[rows, colums];
+    FillArray(array, 0, 10);
+    Console.WriteLine("Заданный массив ");
+    PrintArray(array);
+
+    for (int j = 0; j < colums; j++) 
+    {
+        double sum = 0;
+        for (int i = 0; i < rows; i++)
+        {
+            sum = sum + array[i, j];   
+        }
+        Console.WriteLine("Среднее арифметическое элементов " + j + " - го столбца равна " + Math.Round(sum/rows, 1));
+    }
+}
+
 void FillArrayDouble(double[,] array) 
 {
     Random random = new Random();
@@ -36,12 +80,45 @@ void PrintArrayDouble(double[,] array)
     {
         for (int j = 0; j < array.GetLength(1); j++)
         {
-              Console.Write(Math.Round(array[i, j], 2) + "\t");
+              Console.Write(Math.Round(array[i, j], 1) + "\t");
         }
         Console.WriteLine();
     }    
 }
 
-Zadacha47();
+void FillArray(int[,] array, int startNumber, int finishNumder)
+{
+    Random random = new Random();
+    finishNumder++;
+    int rows =  array.GetLength(0);
+    int colums = array.GetLength(1);
+
+    for (int i = 0; i < rows; i++) 
+    {
+        for (int j = 0; j < colums; j++)
+        {
+            array[i, j] = new Random().Next(startNumber, finishNumder);   
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    int rows =  array.GetLength(0);
+    int colums = array.GetLength(1);
+
+    for (int i = 0; i < rows; i++) 
+    {
+        for (int j = 0; j < colums; j++)
+        {
+              Console.Write(array[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+//Zadacha47();
+//Zadacha50();
+Zadacha52();
 
 
